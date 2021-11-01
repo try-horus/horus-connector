6d32c68208c28d621f542c944a637a4c4bd2caf5
juan-gm <46029286+juan-gm@users.noreply.github.com>
2021-11-01 18:45:34 +0100

    Log the interesting info from a span

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ app.get("/", (req, res) => {
 })
 
 app.post('/v1/traces', (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2))
+  //console.log(req.body.resourceSpans[0]["instrumentationLibrarySpans"])
+  const allSpansArray = req.body.resourceSpans[0]["instrumentationLibrarySpans"]
+  allSpansArray.forEach(element => {
+    const multiLibrarySpans = element.spans
+
+    multiLibrarySpans.forEach(span => {
+      console.log(span.traceId)
+      console.log(span.spanId)
+      console.log(span.parentSpanId)
+      console.log(span.startTimeUnixNano)
+      console.log(span.endTimeUnixNano)
+      // We don't know what are the span
+      // tags and the resource tags
+      console.log("-------------------")
+    })
+  });
   res.send("ok")
 })
 
@@ -20,22 +35,6 @@ app.post('/v1/metrics', (req, res) => {
   console.log(JSON.stringify(req.body, null, 2))
   console.log("NEW REQUEST!!!!")
   res.type('json')
-})
-
-app.post("/", (req, res) => {
-  console.log("THIS IS A REGULAR POST")
-  let request = req.body
-  if (request.resourceMetrics && request.resourceMetrics[0]) {
-    console.log("This is resources")
-    console.log(response.resourceMetrics[0].resource)
-  } else if (request.resourceSpans) {
-    console.log("This is SPANS")
-    console.log(request.resourceSpans[0].resource)
-  } else {
-    console.log("This is else")
-    console.log(request)
-  }
-  res.send("ok")
 })
 
 app.listen(port, () => console.log("Listening on the port"))
