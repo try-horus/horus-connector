b95c3a7ce4ab76ca7f294a72b60ff02fe37e8eef
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-11-02 18:23:31 -0400

    Insert metric statement done

--- index.js
+++ index.js
@@ -21,16 +21,17 @@ app.get("/", (req, res) => {
 
 app.post('/v1/traces', (req, res) => {
   const allSpansArray = req.body.resourceSpans[0]["instrumentationLibrarySpans"]
+  const text = 'INSERT into spans(span_id, trace_id, parent_span_id, start_time, end_time, span_tags) VALUES($1, $2, $3, $4, $5, $6) RETURNING *'
+  
    allSpansArray.forEach(element => {
     const multiLibrarySpans = element.spans
-    const text = 'INSERT into spans(span_id, trace_id, parent_span_id, start_time, end_time, span_tags) VALUES($1, $2, $3, $4, $5, $6) RETURNING *'
     multiLibrarySpans.forEach(span => {
       const values = [span.spanId, span.traceId, span.parentSpanId, span.startTimeUnixNano, span.endTimeUnixNano, JSON.stringify(span.attributes)]
       client.query(text, values, (err, res) => {
         if (err) {
           console.log(err.stack)
         } else {
-          //console.log(res.rows)
+          console.log(res.rows[0])
         }
       })
     });
@@ -40,11 +41,26 @@ app.post('/v1/traces', (req, res) => {
 
 app.post('/v1/metrics', (req, res) => {
   console.log(JSON.stringify(req.body, null, 2))
-  const allMetricsObject = req.body.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[0]
-  const dataPoints = allMetricsObject.doubleSum.dataPoints[0]
+  const allMetricsArray = req.body.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics
   //console.log("FEAR ME I AM THE METRICS OBJECT", allMetricsObject, dataPoints)
-  
   const text = 'INSERT INTO metricstable(name, description, start_time, value, labels) VALUES($1, $2, $3, $4, $5) RETURNING *'
+
+  allMetricsArray.forEach(metric => {
+    const dataPoints = metric.doubleSum.dataPoints[0]
+    const values = [metric.name, metric.description, dataPoints.startTimeUnixNano, dataPoints.value, JSON.stringify(dataPoints.labels)]
+
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log(res.rows[0])
+      }
+    })
+  })
+
+
+
+
 
 
   res.type('json')
