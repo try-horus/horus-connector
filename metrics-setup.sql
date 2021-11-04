3639123dfd348ae59ba8930e828b9b3612e0b79c
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-11-04 10:09:30 -0700

    Renamed metric sql file & added new metric tables

---
+++ metrics-setup.sql
@@ -0,0 +1,30 @@
+/* How to run file
+  1. In postgres: \i <file path>
+  2. In the CLI with psql: psql -d postgres -f <file name>
+*/
+
+CREATE TABLE rps (
+  name varchar NOT NULL,
+  description varchar,
+  start_time bigint NOT NULL,
+  value int NOT NULL,
+  labels jsonb
+);
+
+CREATE TABLE eps (
+  name varchar NOT NULL,
+  description varchar,
+  start_time bigint NOT NULL,
+  value int NOT NULL,
+  labels jsonb
+);
+
+/*
+CREATE TABLE rps_per_endpoint (
+  name varchar NOT NULL,
+  description varchar,
+  start_time bigint NOT NULL,
+  value int NOT NULL,
+  labels jsonb
+);
+*/
