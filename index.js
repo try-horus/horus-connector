93d2358766563fd3a6bdc62e5b9f6daf55a4d450
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-11-17 11:52:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ client.connect()
   .catch(error => console.log(error))
 
 app.use(express.json({limit: '25mb'}));
-app.use(express.urlencoded({limit: '25mb'}));
 app.use(cors())
 
 const port = 3002
